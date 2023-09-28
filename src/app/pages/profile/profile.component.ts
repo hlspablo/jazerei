5339c7aa6a771e65d073e30aa064e513d639c023
Pablo Henrique <pabllobeg@gmail.com>
2023-09-28 15:51:15 -0300

    Profile Page Design OK

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,20 +1,13 @@
 import { Component, Input, OnInit } from "@angular/core"
-import { RxState } from "@rx-angular/state"
-
-//interface State {}
 
 @Component({
   selector: "app-profile",
   templateUrl: "./profile.component.html",
   styleUrls: ["./profile.component.scss"],
-  providers: [RxState],
 })
 export class ProfilePageComponent implements OnInit {
   @Input() id: string
 
-  ngOnInit() {
-    this.fillStars(0)
-  }
   stars: string[] = []
   selectedRating = 0
 
@@ -22,22 +15,14 @@ export class ProfilePageComponent implements OnInit {
     this.stars = Array(5)
       .fill("star_border")
       .map((_, index) => (index < starCount ? "star" : "star_border"))
-    console.log(this.stars)
-  }
-
-  resetStars() {
-    if (this.selectedRating === 0) {
-      this.stars.fill("star_border")
-    } else {
-      this.fillStars(this.selectedRating)
-    }
-  }
-
-  hoverStar(starCount: number) {
-    this.fillStars(starCount)
   }
 
   setRating(rating: number) {
-    this.hoverStar(rating)
+    this.selectedRating = rating
+    this.fillStars(rating)
+  }
+
+  ngOnInit() {
+    this.fillStars(this.selectedRating)
   }
 }
